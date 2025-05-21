7b27eee7c997834a87e41727a32b60dd5c3645cf
cyj <chldydwnsdldpdy1@gmail.com>
2025-05-21 16:49:40 +0900

    두 번째 Board 파일 생성

---
+++ src/com/pcwk/ehr/board/Board2.java
@@ -0,0 +1,10 @@
+package com.pcwk.ehr.board;
+
+public class Board2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Board2 파일 생성");
+	}
+
+}
