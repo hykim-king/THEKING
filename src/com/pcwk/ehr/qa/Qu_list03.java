2588af657ebdea2de2259366cb3e3c0b1e5a6166
ms-ji <jjjiun75@gmail.com>
2025-05-22 16:15:00 +0900

    Qu_list03 업데이트

--- src/com/pcwk/ehr/qa/Qu_list03.java
+++ src/com/pcwk/ehr/qa/Qu_list03.java
@@ -4,6 +4,8 @@ public class Qu_list03 {
 
 	public static void main(String[] args) {
 		System.out.println("오늘은 3번째 업데이트 ~");
+		System.out.println("오늘은 4번째 업데이트");
+		System.out.println("데이터 추가");
 		
 		
 
