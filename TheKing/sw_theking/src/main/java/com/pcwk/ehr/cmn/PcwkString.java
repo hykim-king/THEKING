0550c7d3200fb76ca4315df1c68a82bcb5093b78
koo <qhsghddl33@naver.com>
2025-07-09 01:06:43 +0900

    축제리스트 페이징 selected 해야함

--- TheKing/sw_theking/src/main/java/com/pcwk/ehr/cmn/PcwkString.java
+++ TheKing/sw_theking/src/main/java/com/pcwk/ehr/cmn/PcwkString.java
@@ -6,6 +6,98 @@ import com.pcwk.ehr.user.domain.UserDTO;
 public class PcwkString {
 	
 	/**
+	 * 
+	 * @param maxNum : 총 글수
+	 * @param currentPageNo: 현재 페이지번호
+	 * @param rowPerPage : 페이지 사이즈 (10,20,...100)
+	 * @param bottomCount: 10/5
+	 * @param url : 서버 호출 URL   
+	 * @param scriptName : 자바스크립트 함수명
+	 * @return "html 텍스트"
+	 */
+	public static String renderingPager(int maxNum, int currentPageNo, int rowPerPage, int bottomCount, String url, String scriptName) {
+		StringBuilder  html=new StringBuilder(2000);
+		
+ 
+		
+		//maxNum : 총 글수 -> 21
+		//currentPageNo = 1
+		//rowPerPage : 10
+		//bottomCount: 10
+		
+		int maxPageNo = (maxNum -1) / rowPerPage +1; //3
+		int startPageNo = ((currentPageNo - 1)/bottomCount) * bottomCount +1;//1,11,21,31,...
+		int endPageNo   = ((currentPageNo - 1)/bottomCount+1) * bottomCount;//10,20,30,40,...
+		
+		long nowBlockNo = ((currentPageNo - 1)/bottomCount)+1;//1
+        long maxBlockNo = ((maxNum-1)/bottomCount)+1;//3				
+		
+		
+        if(currentPageNo > maxPageNo) {
+        	return "";
+        }
+        
+		//«	<	1	2	3	4	5	>	»
+		//a b           c           d   e
+		//« : 1 page로 이동
+		//< : 왼쪽으로 bottomCount--
+		//1	2	3	4	5...10 : page번호
+		//> : 오른쪽으로 bottomCount++
+		//» : 마지막 페이지
+        
+        
+        html.append("<div class=\"pagination\"> \n");
+        
+        //« : 1 page로 이동
+        if(nowBlockNo > 1 && nowBlockNo <=maxBlockNo ) {
+        	html.append("<a href=\"javascript:"+scriptName+"('"+url+"',1);\" class=\"prev \">&laquo;</a> \n");        	
+        }
+
+        //< : 왼쪽으로 bottomCount--
+        if(startPageNo > bottomCount) {
+        	html.append("<a href=\"javascript:"+scriptName+"('"+url+"',"+(startPageNo - bottomCount)+");\" >");
+        	html.append("<span>&lt;</span>");
+        	html.append("</a> \n");
+        }
+        
+        //1	2	3	4	5...10 : page번호 
+        int inx = 0;
+        for(inx = startPageNo;inx<=maxPageNo && inx <=endPageNo; inx++) {
+        	if(inx == currentPageNo) {//링크 없음
+        		html.append("<a href=\"#\" title=\"현제 페이지 \" class=\"disabled\">");
+        		html.append(inx);
+        		html.append("</a>\n");
+        	}else {
+        		html.append("<a href=\"javascript:"+scriptName+"('"+url+"',"+(inx)+");\" >");
+        		html.append(inx);
+        		html.append("</a>\n");        		 
+        	}
+        }
+        
+        //> 오른쪽으로 bottomCount++
+        if(maxPageNo > inx) {
+        	html.append("<a href=\"javascript:"+scriptName+"('"+url+"',"+( (nowBlockNo * bottomCount)+1 )+");\" >");
+        	html.append("<span>&gt;</span>");
+        	html.append("</a> \n");        	
+        }
+        
+        //» : 마지막 페이지
+        if(maxPageNo > inx) {
+        	
+        	html.append("<a href=\"javascript:"+scriptName+"('"+url+"',"+( maxPageNo )+");\" >");
+        	html.append("<span>&raquo;</span>");
+        	html.append("</a> \n");           	
+        	
+        }
+        
+
+        html.append("</div>");
+		
+		
+		return html.toString();
+	}	
+	
+	/**
 	 * value가 0이면 defaultValue 리턴
 	 * @param value
 	 * @param defaultValue
